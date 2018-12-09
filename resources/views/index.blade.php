063af7619e7e5b64e0fc566693fd0d97fcde9d9c
Fred <edwardfredparry@gmail.com>
2018-12-09 22:18:54 +0000

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-heading"><label>View User</label>
+            <a class ='pull-right' href="{{ Request::header('referer') }}">
+                <i class="glyphicon glyphicon-circle-arrow-left"></i> Go Back
+            </a>
+        </div>
+    </div>
+@endsection
