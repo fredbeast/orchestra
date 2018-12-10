8eb4d3b019c02dba7406bbe6e73b25c7246e048e
Fred <edwardfredparry@gmail.com>
2018-12-10 12:24:03 +0000

    development - O testing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,20 @@
 @section('content')
     <div class="container py-3">
         <div class="row">
-            <div class ='col-12'>
+            <div class='col-12'>
                 <div class="alert alert-light" role="alert">
                     This site is under maintenance
                 </div>
-
+                <div class="youtube-wrapper mx-auto d-block">
+                    <div class="youtube-middle"></div>
+                    <iframe class="mx-auto d-block" style="margin-left: -100px; margin-top: -90px" width="515" height="500"
+                            src="https://www.youtube-nocookie.com/embed/UnJDbDOLBJc?rel=0&controls=0&autoplay=1&loop=1​&playlist=UnJDbDOLBJc"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                </div>
                 <div class="jumbotron">
-                    <h1 class="display-1">Orchestra</h1>
+                    <h2>Orchestra</h2>
                 </div>
             </div>
         </div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,18 +2,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="We make websites." />
-    <meta name="author" content="Fred Parry" />
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content="We make websites."/>
+    <meta name="author" content="Fred Parry"/>
 
     <title>Orchestra Design</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href = {{ asset("bootstrap/css/bootstrap.css") }} rel="stylesheet" />
-
-    <!-- Compiled CSS -->
+    <!-- Main CSS -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
@@ -22,6 +19,7 @@
 @yield('content')
 @include('includes.footer')
 
+<!-- Main JS -->
 <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
